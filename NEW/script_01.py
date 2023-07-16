e191763c69b85d3ca9dc1d14c9f02378234c14e9
fischerfranz939 <fischerfranz939@gmail.com>
2023-07-16 13:16:33 +0200

    add reporting

--- NEW/script_01.py
+++ NEW/script_01.py
@@ -6,18 +6,111 @@ Script to remove unwanted elements/attributes from XML.
 import xml.etree.ElementTree as ET
 from pathlib import Path
 import time
-import inspect
+import datetime
+import os
 
 
 #INPUT_FILE_NAME = "test_00.osm"
-INPUT_FILE_NAME = "Neuffen_unbearbeitet.osm"
-TEST_PATH = str(Path(__file__).parent.resolve()) + "/"
-FILE_IN  = TEST_PATH + INPUT_FILE_NAME
-FILE_OUT = FILE_IN + ".output"
+INPUT_FILE_NAME = "Neuffen_unbearbeitet.osm"          # file: Daten_00
+PATH = str(Path(__file__).parent.resolve()) + "\\"    # path to file Daten_00
+FILE_IN_NAME  = PATH + INPUT_FILE_NAME
+FILE_OUT_NAME = PATH + "Daten_01.output"              # file: Daten_01
+REPORT_FILE_NAME = PATH + "Daten_01-REPORT.output"
 
 
 #-------------------------------------------------------------------------------
 # Class
+#-------------------------------------------------------------------------------
+class Report:
+    '''Report measurements'''
+    function_name = ""
+    file_name_in  = ""
+    file_name_out  = ""
+    file_report = ""
+    file_size_begin = 0
+    file_size_end = 0
+    time_begin = 0
+    time_end = 0
+
+    def __init__(self, function_name, file_name_in, file_name_out, file_name_report):
+        """Init method"""
+        self.function_name = function_name
+        self.file_name_in = file_name_in
+        self.file_name_out = file_name_out
+        self.file_size_begin = Path(file_name_in).stat().st_size
+        self.file_report = open(file_name_report, "w")
+        # start measurement
+        self.time_begin = round(time.time() * 1000)
+
+    def get_file_name_out(self):
+        '''Get output file name '''
+        return self.file_name_out
+
+    def savings_byte(self):
+        '''Difference between input- and output-file in byte'''
+        return self.file_size_begin - self.file_size_end
+
+    def savings_percent(self):
+        '''Difference between input- and output-file in percent'''
+        savings = 100 - (self.file_size_end/self.file_size_begin) * 100
+        savings = round(savings, 1)
+        return savings
+
+    def time_used_ms(self):
+        '''Time between start and end of measurement'''
+        return self.time_end - self.time_begin
+
+    def stop_measurement(self):
+        '''End of measurement, stop time and get file size'''
+        self.time_end = round(time.time() * 1000)
+        self.file_size_end = Path(self.file_name_out).stat().st_size
+
+    def convert_ms(self):
+        time_used = datetime.datetime.fromtimestamp(self.time_used_ms()/1000.0,
+                                                    tz=datetime.timezone.utc)
+        return "time used      :   " + time_used.strftime('%H:%M:%S.%f') + "\n"
+
+    def convert_bytes(self, bytes, indent=True):
+        '''Convert bytes to kB or MB'''
+        result = ""
+        kilo = 1024
+        indent_x ="           "
+
+        # return bytes
+        if bytes < kilo:
+            result = "byte:   " + str(bytes) + "\n"
+        # return kilo bytes
+        elif bytes >= kilo and bytes < (kilo*kilo):
+            kb = round(bytes/kilo, 1)
+            result = "  kB:   " + str(kb) + "\n"
+        # return mega bytes
+        else:
+            mb = round(bytes/(kilo*kilo), 1)
+            result = "  MB:   " + str(mb) + "\n"
+
+        if indent:
+            result = indent_x + result
+        return result
+
+    def write_report(self):
+        '''Write result into report file'''
+        self.stop_measurement()
+
+        self.file_report.write(">>>>>> " + self.function_name + " <<<<<<\n")
+        #directory = os.path.dirname(self.file_name_in)
+        file_in = os.path.basename(self.file_name_in)
+        #file_out = os.path.basename(self.file_name_out)
+        #self.file_report.write("directory:         " + directory + "\n")
+        self.file_report.write("input file name:   " + file_in + "\n")
+        self.file_report.write(self.convert_bytes(self.file_size_begin))
+        #self.file_report.write("output file name:  " + file_out + "\n")
+        #self.file_report.write(self.convert_bytes(self.file_size_end))
+        self.file_report.write("Savings    ")
+        self.file_report.write(self.convert_bytes(self.savings_byte(), False))
+        self.file_report.write("     in percent:   " + str(self.savings_percent()) + "\n")
+        self.file_report.write(self.convert_ms() + "\n")
+        self.file_report.close()
+
 #-------------------------------------------------------------------------------
 class Counter:
     '''Counts all first level elements per type'''
@@ -73,14 +166,20 @@ class Timer:
 #-------------------------------------------------------------------------------
 def main():
     '''At a first step just count the first level elements per type'''
+    report = Report(str("script_01"), FILE_IN_NAME, FILE_OUT_NAME, REPORT_FILE_NAME)
+    timer = Timer("script_01")
     counter = Counter()
 
-    with open(FILE_OUT, mode="w", encoding="utf-8") as file:
+    print("FILE_IN_NAME:     " + FILE_IN_NAME)
+    print("FILE_OUT_NAME:    " + FILE_OUT_NAME)
+    print("REPORT_FILE_NAME: " + REPORT_FILE_NAME)
+    
+    with open(FILE_OUT_NAME, mode="w", encoding="utf-8") as file:
         file.write("<?xml version='1.0' encoding='UTF-8'?>\n")
         file.write("<osm version='0.6' generator='JOSM'>\n  ")
 
         add_blank = False
-        for element in get_next_first_level_element(FILE_IN):
+        for element in get_next_first_level_element(FILE_IN_NAME):
             counter.count_elements_per_type(element)
 
             ################ actions ################
@@ -107,7 +206,9 @@ def main():
 
         file.write('</osm>' + "\n")
         file.close()
-        counter.print_counter_results()
+    counter.print_counter_results()
+    timer.print_result()
+    report.write_report()
 
 
 #-------------------------------------------------------------------------------
